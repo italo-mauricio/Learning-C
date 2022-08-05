a5b53161f62969e8b3a8f6fec7f786c77317ea43
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-05 03:41:05 -0300

    Valores formatados
    Aula de valores formatados

---
+++ valoresformatados.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int num = 112;
+    printf("%i\n", num );
+
+    float f = 2.2;
+    printf("%f", f);
+
+
+    // integer =  %i ou %d
+    // float ou double = %f
+    // char = %c
+    // string = %s
+    // %u = número inteiro sem o sinal (módulo)
+    // %p = endereço de memória
+    // %e ou %E = imprimir numeros com notação científica
+    // %% = imprimi somente 1 símbolo de porcentagem
+
+    int x = 10;
+    printf("%i\n", x);
+
+    float ff = 4.12;
+    printf("%f\n", ff);
+
+    printf("%i - %f \n", x, ff);
+
+
+    return 0;
+
+
+}
