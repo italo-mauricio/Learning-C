d1669f3e9c5ea61a1aeb7087146e8333b212ff2e
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-05 15:06:08 -0300

    Atualizando entrada de dados

--- entrada_de_dados.c
+++ entrada_de_dados.c
@@ -3,9 +3,17 @@
 
 int main(){
 
-    
 
+    //scanf("tipo_de entrada", variável_que_receberá);
+    //scanf("%tipo1%tipo2", &var1, &var2);
 
+    int num1, num2;
+    printf("Digite um numero: ");
+    scanf("%i", &num1);
+
+    printf("%i", num1); 
+
+    return 0;
 
 
 }
