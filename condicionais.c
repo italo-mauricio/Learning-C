4a353b0147e7a5a8d54c0415560b065d4dc35d03
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-06 11:50:37 -0300

    Estruturas condicionais

---
+++ condicionais.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    int a, b;
+    a = 2;
+    b = 3;
+
+    if (3 > 2){
+        printf("3 eh maior que 2");
+    }
+
+
+    return 0;
+
+}
