d95f0288535032cd02278b74b23602be90e8f441
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-10 18:27:05 -0300

    Função getc

---
+++ função-getc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    char letra;
+    printf("digite um caracter: ");
+    letra = getc(stdin);
+
+
+    printf("Lido: %c\n", letra);
+
+
+
+    return 0;
+
+
+
+}
