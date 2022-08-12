2ab79ef76113196506bc0a2a8f24afcc0989011b
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 01:53:55 -0300

    Update funções.c

--- funções.c
+++ funções.c
@@ -14,7 +14,11 @@ int main()
     resultado = multiplicacao(v1, v2);
     printf("O resultado: %d \n", resultado);
 
-  
+    /*
+        Função em C, funciona basicamente da mesma forma que em Python, você deve declarar a função e depois chamar ela.
+
+
+    */
     return 0;
 }
 
