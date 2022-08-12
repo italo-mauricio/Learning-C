aef696383f58f4cb03b511769dd106672042447e
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 02:12:09 -0300

    Update conversor_bases_numericas.c

--- conversor_bases_numericas.c
+++ conversor_bases_numericas.c
@@ -11,8 +11,18 @@ int main(int argc, char *argv[])
 
     printf("Conversor de bases numericas\n");
     printf("1 = Decimal para Hexadecimal\n");
-    printf("2 = Hexadecimal para Decimal\n")
+    printf("2 = Hexadecimal para Decimal\n");
+    printf("\n\nInforme a opcao: ");
+    scanf("%d", &opcao);
+    getchar();
 
+    if (opcao == 1);
+    {
+        printf("Informe o valor em decimal: ");
+        scanf("%d", &valor);
+        getchar();
+        printf("%d em Hexadecimal eh: %x", valor, valor);
+    }
 
 
 
