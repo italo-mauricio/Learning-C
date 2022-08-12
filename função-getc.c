97219ca00e71b9579b8f59b4a233859b9564b923
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 01:49:57 -0300

    Update função-getc.c

--- função-getc.c
+++ função-getc.c
@@ -4,14 +4,12 @@
 
 int main(){
 
-    char letra;
+    char letra; // Aqui é uma variável do tipo caracter
     printf("digite um caracter: ");
-    letra = getc(stdin);
+    letra = getc(stdin); // getc é uma função que recebe dados do teclado e armazena em uma variável
 
 
-    printf("Lido: %c\n", letra);
-
-
+    printf("Lido: %c\n", letra); // Após letra receber getc, vc pode lê-lo com o printf.
 
     return 0;
 
