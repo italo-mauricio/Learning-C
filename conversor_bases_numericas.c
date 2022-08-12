ef00e54911b0026006a96726a07e99529e79e7ba
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 02:16:55 -0300

    Update Bases numéricas

--- conversor_bases_numericas.c
+++ conversor_bases_numericas.c
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 
 {
 
-    int opcao;
-    int valor; 
+    int opcao; // Aqui são criadas duas variáveis do tipo inteiro.
+    int valor; // Vriável valor vai armazenar os valores que o usuário deseja conveter.
 
     printf("Conversor de bases numericas\n");
     printf("1 = Decimal para Hexadecimal\n");
@@ -16,14 +16,22 @@ int main(int argc, char *argv[])
     scanf("%d", &opcao);
     getchar();
 
-    if (opcao == 1);
+    if (opcao == 1)
     {
         printf("Informe o valor em decimal: ");
         scanf("%d", &valor);
         getchar();
         printf("%d em Hexadecimal eh: %x", valor, valor);
     }
+    else if(opcao == 2)
+    {
+        printf("\nInforme o valor em Hexadecimal: ");
+        scanf("%x", &valor);
+        getchar();
+        printf("%x em decimal eh: %d", valor, valor);
 
+    }
+    else printf("\nValor invalido");
 
 
 
