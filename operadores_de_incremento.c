602ca406752fd2837bf0bbaf6d6c79941886a141
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 01:58:42 -0300

    Create operadores_de_incremento.c

---
+++ operadores_de_incremento.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    /*
+
+    i += 2;, é equivalente a i = i + 2;
+    a -= 3;, é equivalente a a = a - 3;
+    k >>= 3;, é equivalente a k = k >> 3;
+    z &= flag; é equivalente a z = z & flag;
+
+
+
+
+
+    */
+
+
+
+
+    return 0;
+}
