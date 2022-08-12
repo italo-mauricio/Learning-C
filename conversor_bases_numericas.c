828afc4226dd370fcaebe0642c9e08880447d5bc
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-12 02:09:03 -0300

    Create conversor_bases_numericas.c

---
+++ conversor_bases_numericas.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char *argv[])
+
+{
+
+    int opcao;
+    int valor; 
+
+    printf("Conversor de bases numericas\n");
+    printf("1 = Decimal para Hexadecimal\n");
+    printf("2 = Hexadecimal para Decimal\n")
+
+
+
+
+
+    return 0;
+}
