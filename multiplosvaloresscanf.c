44cd57ee6e9a9a2c44dc1bc05b35a8232c40aff1
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-29 01:00:38 -0300

    update

---
+++ multiplosvaloresscanf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+
+        int num1, num2, num3;
+
+        printf("Digite tres numeros inteiros: ");
+        scanf("%d%d%d", &num1, &num2, &num3);
+
+        printf("Valores lidos: %d,%d e %d\n", num1, num2, num3 );
+
+
+
+
+    return 0;
+}
