338c984e29ef6d97f4f4cdccd40de9304e094fbe
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-29 12:32:27 -0300

    update

--- lerdoiscaracteres.c
+++ lerdoiscaracteres.c
@@ -14,6 +14,7 @@ int main(){
     printf("Primeira letra: %c\nSegunda letra: %c\n", a, b);
 
     // o espaço entre os %c é o comando para  computador desconsiderar o ENTER, o TAB e o ESPAÇO
+    // fflush(); <- Não use
     return 0;
 
 }
