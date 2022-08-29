17ddae9042e6b911506c4832d64f8a2647bbeb9a
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-29 13:28:52 -0300

    Trabalhando com short int

--- operador-short.c
+++ operador-short.c
@@ -5,18 +5,25 @@
 
      utilzando operador short para o tipo int
      intervalo: -32.768 até 32.767
-     %hi u %d
+     %hi ou %d
+
+    short é utilizado para economizar memoria
 
 
+    O short só pode ser utilizado neste intervalo de numeros, caso voce queira usar um intervalo maior
+    ou menor tera que utilzar um int normal sem os short.
+    
 */
 
 
 int main(){
 
-    float x = 1.0;
+    int y = 0;
+    short int x = 32767;
 
-    printf("Tamanho de um float na memoria: %d bytes\n", sizeof (x));
-    printf("Tamanho em memoria de um int: %d bytes\n", sizeof(int));
+    printf("Valor de x: %hi\n", x);
+    x++;
+    printf("Valor de x: %d\n", x);
 
     // 4 * 8 = 32 bits
 
@@ -24,3 +31,4 @@ int main(){
     return 0;
 
 }
+
