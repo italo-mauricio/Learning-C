3247eca8b01fbc02a55c17d1f233a160472008c7
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-30 18:00:23 -0300

    usando caracteres de escape

---
+++ Arquivos/caracteres-escape.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/*
+            Aula 23
+
+    Caracteres de escape
+    Quebra de linha -> \n
+    Tabulação       -> \t
+    Aspa dupla      -> \"
+    Aspa simples    -> \'
+    Contrabarra     -> \\
+
+
+*/
+
+int main(){
+
+    printf("\thi \"there\".\n\n");   // usamos \" para destacar um caracter sem interferir no printf
+
+
+    return 0;
+}
