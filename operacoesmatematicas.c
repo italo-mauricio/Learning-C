066badda927f1efcea4be482e42083d29d48e5f4
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-08-30 18:09:33 -0300

    operadores matemáticos

---
+++ operacoesmatematicas.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/*
+
+        usando operadores matemáticos em C
+
+        soma = +
+        subtração = -
+        multiplicação = *
+        divisão = /
+
+
+*/
+
+int main(){
+
+
+    printf("\nsoma: %d\n", 10 + 20);
+
+
+    return 0;
+
+}
