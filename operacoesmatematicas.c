51f16a9811d8f1279b17e152eb91b90aeb27f9d5
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-07 21:13:35 -0300

    update

--- operacoesmatematicas.c
+++ operacoesmatematicas.c
@@ -20,9 +20,11 @@ int main(){
     printf("\nsoma: %d\n", 10 + 20);
     printf("\nSubtracao: %d\n", 10 - 20);
     printf("\nMultiplicacao: %d\n", 10 * 20);
-    printf("\nDivisao: %d\n", 10 / 20);
+    printf("\nDivisao: %f\n", 10.0 / 20);
 
 
     return 0;
 
 }
+
+
