cd5c2370f4856d088bb6bfebb04b31d5e72395a3
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-07 21:34:44 -0300

    trabalhando com resto de divisão

---
+++ Arquivos/restodadivisao.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/*
+
+    Usando operadores matemáticos em C
+
+    Trabalhando com resto da divisão
+            
+
+
+*/
+
+int main(){
+
+
+    int a = 10, b = 20;
+
+    // 10 / 20 r = 10
+    // 20 / 10 r= 0
+
+    printf("Resto da divisão inteira: %d\n", a % b);
+
+
+    return 0;
+
+}
+
+
