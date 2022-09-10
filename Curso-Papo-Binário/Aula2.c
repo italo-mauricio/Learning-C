a66e53da66f42b233f017ae2673f060ec733921c
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-10 19:09:44 -0300

    Verificando bits

--- Curso-Papo-Binário/Aula2.c
+++ Curso-Papo-Binário/Aula2.c
@@ -10,10 +10,16 @@
 */
 
 
-int main(){
+int main(void){
+    char c;
+    float f = 13.4;
 
+    printf("O tamanho de C eh: %lu\n", sizeof c);       // sizeof retorna o tamanho da variável retornando um número.
+    printf("O tamanho de F eh: %f\n", sizeof f);
+    printf("O tamanho de C eh: %lu bytes / %lu bits\n", sizeof c, sizeof c *8);
 
-
-
+    return 0;
 
 }
+
+// sizeof não é uma função, é um operador
