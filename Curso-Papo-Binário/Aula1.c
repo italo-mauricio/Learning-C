f09527a8b60aa209a2afe54324b14cbe8a88aa9a
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-10 17:51:40 -0300

    verificando o retorno da variável empregada

--- Curso-Papo-Binário/Aula1.c
+++ Curso-Papo-Binário/Aula1.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 /*
 
         Aula 1, sobre printf e funções
-
+        %d = decimal
 
 
 */
-
-
-
 
 int main(void){
 
     int ret = 0;
 
-    ret = printf("Ola mundo");
+    ret = printf("Ola mundo\n");
+    printf("retorno da printf anterior %d\n", ret);  // verificando o retorno da variável ret
 
     return 0;
 
