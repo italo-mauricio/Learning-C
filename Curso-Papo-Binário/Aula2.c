0f8be2f79424f23e22922f8651fecc65e810ee5a
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-13 14:11:35 -0300

    trabalhando com char

--- Curso-Papo-Binário/Aula2.c
+++ Curso-Papo-Binário/Aula2.c
@@ -11,15 +11,24 @@
 
 
 int main(void){
-    char c;
+    signed char c1;
+    signed char c2;
+    signed char c3;
     float f = 13.4;
 
-    printf("O tamanho de C eh: %lu\n", sizeof c);       // sizeof retorna o tamanho da variável retornando um número.
+    printf("O tamanho de C eh: %lu\n", sizeof c1);       // sizeof retorna o tamanho da variável retornando um número.
     printf("O tamanho de F eh: %f\n", sizeof f);
-    printf("O tamanho de C eh: %lu bytes / %lu bits\n", sizeof c, sizeof c *8);
+    printf("O tamanho de C eh: %lu bytes / %lu bits\n", sizeof c1, sizeof c1 *8);
 
-    c = 255;
-    printf("Valor de c: %d\n", c);
+    c1 = 10;
+    c2 = 0xa;
+    c3 = '\n';
+    printf("Valor de c: %d\n", c1);
+    printf("Valor de c: %d\n", c2);
+    printf("Valor de C: %d\n", c3);
+
+
+
 
     return 0;
 
