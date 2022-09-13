5d1f8ad986a82183c2526bbac0e5e4a071e09553
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-13 14:21:00 -0300

    adicionando a parte 2 da aula 2

---
+++ Curso-Papo-Binário/Aula2-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// trabalhando com booleano
+
+int main(){
+
+    bool b;
+    char c;
+
+    c = 0; // false
+    c = 1; // true
+
+
+    b = true;
+    b = false;
+
+    printf("O tamanho de b: %lu\n", sizeof b);
+
+
+    return 0;
+
+
+
+}
