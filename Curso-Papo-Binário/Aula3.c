2631ad0620442d67a2039ea3a064196e34a02745
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-13 14:42:40 -0300

    Usando o include limits

--- Curso-Papo-Binário/Aula3.c
+++ Curso-Papo-Binário/Aula3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 
 // Aul 3, variáveis do tipo int
@@ -8,10 +9,10 @@
 
 int main(){
 
-    int i;
+    int i = INT_MAX;
 
     printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
-    printf("Valor de i: %d\n"), i;
+    printf("Valor de i: %d\n", i);
 
 
 
