77464d950fb3e183e1931638dee1384af6d6cd4f
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-13 14:38:12 -0300

    adicionando a aula 3, trabalhando com inteiros

---
+++ Curso-Papo-Binário/Aula3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Aul 3, variáveis do tipo int
+
+
+
+int main(){
+
+    int i;
+
+    printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
+    printf("Valor de i: %d\n"), i;
+
+
+
+
+
+    return 0;
+}
