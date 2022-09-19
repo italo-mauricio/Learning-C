b492ea0daecd1f73a1ae639b61faccc0a4516ab2
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-19 12:47:01 -0300

    update

--- Curso-Papo-Binário/Aula3.c
+++ Curso-Papo-Binário/Aula3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
+#include <stdint.h>
 
 // Aul 3, variáveis do tipo int
 
@@ -9,15 +9,16 @@
 
 int main(){
 
-    unsigned int i = UINT_MAX;
-
+    // uint32_t i = USHRT_MAX;     // usando uint32_t eu garanto que aquela variável terá 32 bits
+    // uint8_t i = UCHAR_MAX;
+    short int i;
     // i += 2; // i = i + 1 ou i += 1  é a mesma coisa
 
     printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
     printf("Valor de i: %u\n", i);
     printf("Valor de i: %x\n", i);
     printf("Valor de i: %o\n", i);
-
+    
 
 
 
