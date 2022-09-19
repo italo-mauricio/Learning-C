603d3c55f72841d773894a4d933baff4c286a32a
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-19 12:49:27 -0300

    update

--- Curso-Papo-Binário/Aula3.c
+++ Curso-Papo-Binário/Aula3.c
@@ -11,7 +11,8 @@ int main(){
 
     // uint32_t i = USHRT_MAX;     // usando uint32_t eu garanto que aquela variável terá 32 bits
     // uint8_t i = UCHAR_MAX;
-    short int i;
+    // short int i;
+    long long i;
     // i += 2; // i = i + 1 ou i += 1  é a mesma coisa
 
     printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
