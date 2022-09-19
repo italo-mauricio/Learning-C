dbdfeeacdfac00172557f10b0f51fe7bb2dedfcb
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-19 10:44:40 -0300

    update

--- Curso-Papo-Binário/Aula3.c
+++ Curso-Papo-Binário/Aula3.c
@@ -5,13 +5,13 @@
 
 // Aul 3, variáveis do tipo int
 
-
+// comando cat verifica o código todo
 
 int main(){
 
     int i = INT_MAX;
 
-    i++; // i = i + 1 ou i += 1  é a mesma coisa
+    i += 2; // i = i + 1 ou i += 1  é a mesma coisa
 
     printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
     printf("Valor de i: %d\n", i);
