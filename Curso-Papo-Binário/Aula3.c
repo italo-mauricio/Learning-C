c83ea2e9788bf61082ff6e9a21013a3a64407ba2
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-21 15:32:51 -0300

    aprendendo sobre int

--- Curso-Papo-Binário/Aula3.c
+++ Curso-Papo-Binário/Aula3.c
@@ -12,13 +12,16 @@ int main(){
     // uint32_t i = USHRT_MAX;     // usando uint32_t eu garanto que aquela variável terá 32 bits
     // uint8_t i = UCHAR_MAX;
     // short int i;
-    long long i;
+    // long long i = ULONG_MAX;
     // i += 2; // i = i + 1 ou i += 1  é a mesma coisa
 
-    printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof i, sizeof i *8);
-    printf("Valor de i: %u\n", i);
-    printf("Valor de i: %x\n", i);
-    printf("Valor de i: %o\n", i);
+    // size_t i;
+
+    register int i = 0;
+
+    printf("O tamanho de i (int): %d bytes / %d bits\n", sizeof i, sizeof i *8);
+    printf("Valor de i: %d\n", i);
+
     
 
 
