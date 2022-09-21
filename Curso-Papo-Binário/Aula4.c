fb2b28c7b6682e3edde6758eb4afc17d01f54c81
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-09-21 15:37:45 -0300

    adicionando a aula 4 sobre float

---
+++ Curso-Papo-Binário/Aula4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+
+    float f = 1;
+
+    printf("O tamanho de f (float): %zu bytes / %zu bits\n", sizeof f, sizeof f *8);
+
+    printf("Valor de f: %f\n", f);
+
+
+
+    return 0;
+
+
+
+
+
+}
+
+
