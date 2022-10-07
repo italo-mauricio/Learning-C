70efb58d7eac492fc371ec0ff6eb4c403b09f3bb
italo <italogithub@gmail.com>
2022-10-07 14:54:10 -0300

    adicionando anotações e aula de modificadores de tipos

---
+++ Curso-Excript/Aula8-modificadores-tipo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    /*
+    
+    1) char
+    2) int
+    3) float
+    4) double
+    5) void - vazio
+
+
+    1) signed     -> + e -
+    2) unsigned   -> +
+    3) long       -> aumentar a capacidade de armazenamento da nossa vairável
+    4) short
+
+
+
+    */
+
+
+
+
+
+
+
+    return 0;
+}
