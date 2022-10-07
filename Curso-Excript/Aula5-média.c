0d6232486550cb038bf6843daaa11802bb35b576
italo <italogithub@gmail.com>
2022-10-07 12:28:07 -0300

    adicionando médias

---
+++ Curso-Excript/Aula5-média.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    double nota1, nota2, nota3, nota4, media;
+    nota1, nota2, nota3, nota4 = 0;
+
+    printf("\t\t\t\tMedia escolha 1.0\n");
+    printf("Digite a sua primeira nota: ");
+    
+
+
+    return 0;
+}
