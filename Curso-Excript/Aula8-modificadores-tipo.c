7fd8f0b076afaf451fc7338206b931b06f183dbb
italo <italogithub@gmail.com>
2022-10-07 15:21:02 -0300

    verificando tamanho das variaveis

--- Curso-Excript/Aula8-modificadores-tipo.c
+++ Curso-Excript/Aula8-modificadores-tipo.c
@@ -4,6 +4,22 @@
 
 int main(){
 
+    int i = 10;
+
+
+    long long a;
+
+    double u;
+
+    printf("%i -> variavel i\n", sizeof(i));
+    printf("%i -> variavel u\n", sizeof(u));
+    printf("%d", sizeof(a));
+
+
+
+
+
+
     /*
     
     1) char
@@ -14,9 +30,9 @@ int main(){
 
 
     1) signed     -> + e -
-    2) unsigned   -> +
+    2) unsigned   -> +   só receberá numeros positivos
     3) long       -> aumentar a capacidade de armazenamento da nossa vairável
-    4) short
+    4) short      -> diminuir o tamanho de armazenamento 
 
 
 
