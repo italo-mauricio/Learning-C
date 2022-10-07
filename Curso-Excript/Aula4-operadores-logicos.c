038a4c01a7974634015e4338d0e7e855f97390e0
italo <italogithub@gmail.com>
2022-10-07 11:40:44 -0300

    update

--- Curso-Excript/Aula4-operadores-logicos.c
+++ Curso-Excript/Aula4-operadores-logicos.c
@@ -3,20 +3,41 @@
 
 
 int main(){
-
+    /*
     int i;
-
+   
     printf("Digite um numero: ");
-    scanf("%d", &i);
+    scanf("%i", &i);
 
     if (i >= 5 && i <= 10){
 
         printf("Entre 5 e 10");
     }else{
 
-        printf("Abaixo de 5 ou maior de 10");
+        printf("Abaixo de 5 ou maior que 10");
     }
-
-
+    
     return 0;
+
+    */
+
+   int i = 10;
+
+   printf("\n\t%i\n", i != 1);
+   printf("\n\t%i\n", i == 1);
+
+
+   return 0;
+
+   /*
+   
+   1) positivo -> 1
+   2) negativo -> 0
+   3) comparação negativa !=
+   4) comparação positiva ==
+   
+   */
+
+
 }
+
