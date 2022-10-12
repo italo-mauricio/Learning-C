ca06e5567abe4d9fd80d91d9a152c09ab81d0e90
italo <italogithub@gmail.com>
2022-10-12 12:29:01 -0300

    update

--- Curso-Excript/calculadora.c
+++ Curso-Excript/calculadora.c
@@ -7,30 +7,54 @@
 
 int main(){
 
-    double num1, num2, resultado;
+    float num1, num2, resultado;
     char op = '0';
 
     do
     {
         num1 = num2 = resultado = 0;
-        printf(" 1 - Somar\n");
+        printf("\n 1 - Somar\n");
         printf(" 2 - Subtrair\n");
         printf(" 3 - Multiplicar\n");
         printf(" 4 - Dividir\n");
+        printf(" 0 - Sair\n");
 
-        printf("Informe a operacao: \n");
+        printf("\nInforme a operacao: \n");
         op = getche();
         printf("\t\t\t>>> ");
-        if (op > '4'){
-            printf("Opcao invalida!\n");
-            return main();
+        if (op != '0'){
+            printf("Digite o primeiro numero:\n\t ");
+            scanf("%f", &num1);
+            printf("Digite o segundo numero:\n\t ");
+            scanf("%f", &num2);
+
+
+            if (op == '1'){
+
+                resultado = num1 + num2;
+             }else{
+                if(op == '2'){
+                    resultado = num1 - num2;
+                }else{
+                    if(op == '3'){
+                        resultado = num1 * num2;
+                    }else{
+                        if (op == '4'){
+                            resultado = num1 / num2;
+                     
+                        }
+                   
+                    }
+               
+                }
+
+            }
+                    
+               
 
         }
-        printf("Digite o primeiro numero:\n\t ");
-        scanf("%f", &num1);
-        printf("Digite o segundo numero:\n\t ");
-        scanf("%f", &num2);
-
+        
+        printf("O resultado eh: %f", resultado);
    
       
     } while (op != '0');
