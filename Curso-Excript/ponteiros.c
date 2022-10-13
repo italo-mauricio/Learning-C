17754f1e24eddbc6060cd75cb9889cabc461aabc
italo <italogithub@gmail.com>
2022-10-13 18:03:59 -0300

    ponteiros

--- Curso-Excript/ponteiros.c
+++ Curso-Excript/ponteiros.c
@@ -4,21 +4,42 @@
 
 int main(){
 
-    int x = 10;
-    int *ponteiro;
-    ponteiro = &x;
+    /*
+    int valor = 27;
 
-    int y = 20;
-    *ponteiro = y; 
+    int *ptr ;
+
+    ptr = &valor;
 
 
+    printf("utilizando ponteiros\n\n");
+    printf("Conteudo da variavel valor: %d\n", valor);
+    printf("Endereco da variavel valor: %x\n", &valor);
+    printf("Conteudo da variavel ponteiro ptr: %x", ptr);
 
-    printf("%i %i\n", x, y );
-
-    getchar();
 
     return 0;
 
+    */
+
+   int x, *ponteiro, **ponteirode;
+   printf("\nValor de x = %d\n", x);
+   printf("Endereco de x: %x\n\n", &x);
+   x = 0;
+   ponteiro = &x;
+   ponteirode = &ponteiro;
+
+
+   printf("\nValor de x = %d", x);
+   printf("\nEndereco apontando por ponteiro: %d", ponteiro);
+   printf("\nValor da variavel x que esta sendo apotando por ponteiro: %d\n", *ponteiro);
+   printf("\nEndereco de memoria da variavel ponteiro %x\n", &ponteiro);
+
+   system("Pause");
+
+
+
+   return 0;
 
 
 
